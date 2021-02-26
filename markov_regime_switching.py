c5be54d058c25983eb81a383267bd52229cd8737
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-26 23:33:40 +0800

    Update markov_regime_switching.py

--- markov_regime_switching.py
+++ markov_regime_switching.py
@@ -8,37 +8,45 @@ Original file is located at
 """
 
 # Commented out IPython magic to ensure Python compatibility.
-!pip install hmmlearn
-!pip install pyforest
 from pyforest import *
 import datetime, pickle, copy
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 150)
+
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 500)
+pd.set_option("display.width", 150)
 import matplotlib.pyplot as plt
-# %matplotlib inline  
+
+# %matplotlib inline
 from pandas.plotting import register_matplotlib_converters
+
 register_matplotlib_converters()
-!pip install quandl
-import quandl
-plt.style.use('ggplot')
-from statistics import variance 
+plt.style.use("ggplot")
+from statistics import variance
 from random import randint
 import scipy as sp
 from scipy import stats
-!pip install ffn
-import ffn
 from matplotlib import cm
 import numpy as np
+
 # %matplotlib inline
-from sklearn.preprocessing import scale # for the check the error and accuracy of the model
+from sklearn.preprocessing import (
+    scale,
+)  # for the check the error and accuracy of the model
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, r2_score
 import sklearn.mixture as mix
 import math, pickle
 from math import sqrt
-from sklearn.model_selection import train_test_split, KFold, StratifiedKFold, cross_val_score, GridSearchCV, cross_validate
+from sklearn.model_selection import (
+    train_test_split,
+    KFold,
+    StratifiedKFold,
+    cross_val_score,
+    GridSearchCV,
+    cross_validate,
+)
 import warnings
 import seaborn as sns
+
 sns.set()
 from hmmlearn.hmm import GaussianHMM
 from random import randint
@@ -46,125 +54,133 @@ from sklearn import mixture
 import itertools
 from scipy import linalg
 import matplotlib as mpl
+
 warnings.filterwarnings("ignore")
+from statsmodels.compat import lzip
+import statsmodels.stats.api as sms
 
 import pandas_datareader as web
-df = web.DataReader('^IXIC', data_source = 'yahoo', start = '2000-01-01')
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+import networkx as nx
+import probscale
+from pprint import pprint
 
-print(f'Nasdaq Composite Index:')
+df = web.DataReader("^IXIC", data_source="yahoo", start="2000-01-01")
+
+print(f"Nasdaq Composite Index:")
 print(df.head())
-print('\n')
+print()
 print(df.shape)
 
-data = df[['High','Low','Open','Adj Close', 'Volume']]
-data['returns'] = np.log(data['Adj Close'] / data['Adj Close'].shift())
+data = df[["High", "Low", "Open", "Adj Close", "Volume"]]
+
+data["returns"] = np.log(data["Adj Close"] / data["Adj Close"].shift())
+
 data.dropna(inplace=True)
-data.tail()
+print(data.tail())
 
-plt.figure(figsize=(14,6))
-plt.subplot(1,2,1)
-data['Adj Close'].hist(bins=50)
-plt.title('Adj Close price')
-plt.subplot(1,2,2)
-stats.probplot(data['Adj Close'], plot=plt);
-data['Adj Close'].describe().T
+plt.figure(figsize=(14, 6))
+plt.subplot(1, 2, 1)
+data["Adj Close"].hist(bins=50)
+plt.title("Adj Close price")
+plt.subplot(1, 2, 2)
+stats.probplot(data["Adj Close"], plot=plt)
+data["Adj Close"].describe().T
 
-from statsmodels.tsa.stattools import adfuller
 
-adfuller(data['returns'].dropna())
+adfuller(data["returns"].dropna())
 
-data['state'] = data['returns'].apply(lambda x: 'up' if (x > 0.001)\
-else ('down' if (x < -0.001)\
-else 'no_change'))
-data.tail()
+data["state"] = data["returns"].apply(
+    lambda x: "up" if (x > 0.001) else ("down" if (x < -0.001) else "no_change")
+)
+print(data.tail())
 
-data['prev_state'] = data['state'].shift(1)
-data.tail()
+data["prev_state"] = data["state"].shift(1)
+print(data.tail())
 
-state_space = data[['prev_state', 'state']]
-state_space_matrix = data.groupby(['prev_state', 'state']).size().unstack()
+state_space = data[["prev_state", "state"]]
+state_space_matrix = data.groupby(["prev_state", "state"]).size().unstack()
 state_space_matrix
 
 print(state_space_matrix.sum())
 
-transition_matrix = state_space_matrix.apply(lambda x: x/float(x.sum()), axis=1)
+transition_matrix = state_space_matrix.apply(lambda x: x / float(x.sum()), axis=1)
 transition_matrix
 
 print(transition_matrix.sum(axis=1))
 
 t0 = transition_matrix.copy()
 t1 = round(t0.dot(t0), 4)
-t1
+print(t1)
 
 t2 = round(t0.dot(t1), 4)
-t2
+print(t2)
 
 t3 = round(t0.dot(t2), 4)
-t3
+print(t3)
 
-pd.DataFrame(np.linalg.matrix_power(t0,4))
+pd.DataFrame(np.linalg.matrix_power(t0, 4))
 
 # find the equilibrium matrix
-i=1
-a= t0.copy()
+i = 1
+a = t0.copy()
 b = t0.dot(t0)
-while(not(a.equals(b))):
-  print('iteration number:' +str(i))
-  i += 1
-  a = b.copy()
-  b = b.dot(t0)
+while not (a.equals(b)):
+    print("iteration number:" + str(i))
+    i += 1
+    a = b.copy()
+    b = b.dot(t0)
 
-from pprint import pprint 
 
-# create a function that maps transition probability dataframe 
+# create a function that maps transition probability dataframe
 # to markov edges and weights
+
 
 def _get_markov_edges(Q):
     edges = {}
     for col in Q.columns:
         for idx in Q.index:
-            edges[(idx,col)] = Q.loc[idx,col]
+            edges[(idx, col)] = Q.loc[idx, col]
     return edges
+
 
 edges_wts = _get_markov_edges(transition_matrix)
 pprint(edges_wts)
 
-import networkx as nx
-
-states = ['up', 'down', 'no_change']
+states = ["up", "down", "no_change"]
 
 # create graph object
 G = nx.MultiDiGraph()
 
 # nodes correspond to states
 G.add_nodes_from(state_space_matrix)
-print(f'Nodes:\n{G.nodes()}\n')
+print(f"Nodes:\n{G.nodes()}\n")
 
 # edges represent transition probabilities
 for k, v in edges_wts.items():
     tmp_origin, tmp_destination = k[0], k[1]
     G.add_edge(tmp_origin, tmp_destination, weight=v, label=v)
-print(f'Edges:')
-pprint(pd.DataFrame(G.edges(data=True)))    
+print(f"Edges:")
+pprint(pd.DataFrame(G.edges(data=True)))
 
-plt.figure(figsize=(12,6))
-pos = nx.drawing.nx_pydot.graphviz_layout(G, prog='dot')
+plt.figure(figsize=(12, 6))
+pos = nx.drawing.nx_pydot.graphviz_layout(G, prog="dot")
 print(nx.draw_networkx(G, pos))
 
 # create edge labels for jupyter plot but is not necessary
-edge_labels = {(n1,n2):d['label'] for n1,n2,d in G.edges(data=True)}
-nx.draw_networkx_edge_labels(G , pos, edge_labels=edge_labels)
-nx.drawing.nx_pydot.write_dot(G, 'nasdaq_markov.dot')
-
-import statsmodels.api as sm
+edge_labels = {(n1, n2): d["label"] for n1, n2, d in G.edges(data=True)}
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+nx.drawing.nx_pydot.write_dot(G, "nasdaq_markov.dot")
 
 # Plot the data
-data['returns'].plot(title='Nasdaq daily return percentage', figsize=(12,5))
+data["returns"].plot(title="Nasdaq daily return percentage", figsize=(12, 5))
 
 data.dropna(inplace=True)
 
-model = sm.tsa.MarkovRegression(data['returns'][-2500:], k_regimes=3,
-                                switching_variance=True)
+model = sm.tsa.MarkovRegression(
+    data["returns"][-2500:], k_regimes=3, switching_variance=True
+)
 
 
 # launch the modelling process on the dataset
@@ -181,108 +197,123 @@ print(res_1.summary())
 
 pred_1 = res_1.predict()
 pred_1 = pd.DataFrame(pred_1).tail(20)
-pred_1.rename(columns ={0: 'Predicted'}, inplace=True)
-com_1 = pd.concat([pred_1, data['returns'].tail(20)], axis=1)
+pred_1.rename(columns={0: "Predicted"}, inplace=True)
+com_1 = pd.concat([pred_1, data["returns"].tail(20)], axis=1)
 com_1 = com_1.reset_index()
-com_1
+print(com_1)
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=com_1['Date'],y=com_1['returns'],
-                         name="Actual returns"))
+fig.add_trace(go.Scatter(x=com_1["Date"], y=com_1["returns"], name="Actual returns"))
 
-fig.add_trace(go.Scatter(x=com_1['Date'],y=com_1['Predicted'],
-                         name="Predicted return"))
+fig.add_trace(
+    go.Scatter(x=com_1["Date"], y=com_1["Predicted"], name="Predicted return")
+)
 
-fig.update_layout(title="Nasdaq Actual vs Predicted returns",
-   yaxis_title="Price ($)",
-    font=dict(family="Courier New, monospace",size=18,color="#7f7f7f"))
-fig.update_layout(autosize=False,width=800,height=400,)
+fig.update_layout(
+    title="Nasdaq Actual vs Predicted returns",
+    yaxis_title="Price ($)",
+    font=dict(family="Courier New, monospace", size=18, color="#7f7f7f"),
+)
+fig.update_layout(
+    autosize=False,
+    width=800,
+    height=400,
+)
 fig.update_layout(legend_orientation="h")
 fig.show()
 
-fig, axes = plt.subplots(3, figsize=(10,7))
+fig, axes = plt.subplots(3, figsize=(10, 7))
 ax = axes[0]
 ax.plot(res_1.smoothed_marginal_probabilities[0])
-#ax.fill_between(data['returns'].index, 0,  where=data['returns'].values, color='gray', alpha=0.3)
-ax.set(title='Smoothed probability of down regime for Nasdaq returns')
+# ax.fill_between(data['returns'].index, 0,  where=data['returns'].values, color='gray', alpha=0.3)
+ax.set(title="Smoothed probability of down regime for Nasdaq returns")
 ax = axes[1]
 ax.plot(res_1.smoothed_marginal_probabilities[1])
-ax.set(title='Smoothed probability of no_change regime for Nasdaq returns')
+ax.set(title="Smoothed probability of no_change regime for Nasdaq returns")
 ax = axes[2]
 ax.plot(res_1.smoothed_marginal_probabilities[2])
-ax.set(title='Smoothed probability of up-variance regime for Nasdaq returns')
+ax.set(title="Smoothed probability of up-variance regime for Nasdaq returns")
 fig.tight_layout()
 
 print(res_1.expected_durations)
 
-data['volume_gap'] = np.log(data['Volume'] / data['Volume'].shift()) 
-data['daily_change'] = (data['Adj Close'] - data['Open']) / data['Open']
-data['fract_high'] = (data['High'] - data['Open']) / data['Open']
-data['fract_low'] = (data['Open'] - data['Low']) / data['Open']
-data['forecast_variable'] = (data['Adj Close'].shift(-1) - data['Open'])
+data["volume_gap"] = np.log(data["Volume"] / data["Volume"].shift())
+
+data["daily_change"] = (data["Adj Close"] - data["Open"]) / data["Open"]
+
+data["fract_high"] = (data["High"] - data["Open"]) / data["Open"]
+
+data["fract_low"] = (data["Open"] - data["Low"]) / data["Open"]
+
+data["forecast_variable"] = data["Adj Close"].shift(-1) - data["Open"]
+
 data.dropna(inplace=True)
+
 data = data[~data.isin([np.nan, np.inf, -np.inf]).any(1)]
-endog = data['forecast_variable'][-2500:]
-exog = data [['volume_gap', 'daily_change', 'fract_high', 'fract_low']][-2500:]
+
+endog = data["forecast_variable"][-2500:]
+
+exog = data[["volume_gap", "daily_change", "fract_high", "fract_low"]][-2500:]
 
 # Fit the 2-regime model
-mod_2 = sm.tsa.MarkovRegression(
-    endog=endog, k_regimes=3, exog=exog)
+mod_2 = sm.tsa.MarkovRegression(endog=endog, k_regimes=3, exog=exog)
 res_2 = mod_2.fit(search_reps=50)
 print(res_2.summary())
 
 res_2.smoothed_marginal_probabilities[0].plot(
-    title='Probability of being in a low-variance regime', figsize=(12,5));
+    title="Probability of being in a low-variance regime", figsize=(12, 5)
+)
 
-fig, axes = plt.subplots(3, figsize=(10,7))
+fig, axes = plt.subplots(3, figsize=(10, 7))
 
 ax = axes[0]
 ax.plot(res_2.smoothed_marginal_probabilities[0])
-ax.set(title='Smoothed probability of down regime')
+ax.set(title="Smoothed probability of down regime")
 
 ax = axes[1]
 ax.plot(res_2.smoothed_marginal_probabilities[1])
-ax.set(title='Smoothed probability of no_change regime')
+ax.set(title="Smoothed probability of no_change regime")
 
 ax = axes[2]
 ax.plot(res_2.smoothed_marginal_probabilities[2])
-ax.set(title='Smoothed probability of up regime')
+ax.set(title="Smoothed probability of up regime")
 
 plt.tight_layout()
 
-!pip install probscale
-import probscale
 
-#qqpolot vs. normal distribution
+# qqpolot vs. normal distribution
 fig, ax = plt.subplots(figsize=(10, 4))
 plt.grid(True)
-fig = probscale.probplot(res_2.resid, ax=ax, plottype='pp', bestfit=True,
-                         problabel='Percentile', datalabel='Residuals',
-                         scatter_kws=dict(label='Model residuals'),
-                         line_kws=dict(label='Best-fit line'))
-ax.legend(loc='upper left')
+fig = probscale.probplot(
+    res_2.resid,
+    ax=ax,
+    plottype="pp",
+    bestfit=True,
+    problabel="Percentile",
+    datalabel="Residuals",
+    scatter_kws=dict(label="Model residuals"),
+    line_kws=dict(label="Best-fit line"),
+)
+ax.legend(loc="upper left")
 plt.show()
 
-import seaborn as sns
-plt.figure(figsize=(12,5))
+plt.figure(figsize=(12, 5))
 
 # Plot a simple histogram with binsize determined automatically
 sns.distplot(res_2.resid, 20)
-plt.title('Histogram of residuals')
-plt.xlabel('Residuals')
-plt.ylabel('Density')
+plt.title("Histogram of residuals")
+plt.xlabel("Residuals")
+plt.ylabel("Density")
 plt.grid(True)
 plt.show()
 
-from statsmodels.compat import lzip
-import statsmodels.stats.api as sms
 
-name = ['Lagrange multiplier statistic', 'p-value','f-value', 'f p-value']
+name = ["Lagrange multiplier statistic", "p-value", "f-value", "f p-value"]
 results1 = sms.acorr_breusch_godfrey(res_2, 10)
 print(lzip(name, results1))
 
-name = ['Jarque-Bera', 'Chi^2 two-tail prob.', 'Skew', 'Kurtosis']
-JB, JBpv,skw,kurt = sm.stats.stattools.jarque_bera(res_2.resid)
+name = ["Jarque-Bera", "Chi^2 two-tail prob.", "Skew", "Kurtosis"]
+JB, JBpv, skw, kurt = sm.stats.stattools.jarque_bera(res_2.resid)
 print(lzip(name, results1))
 
 print(res_2.expected_durations)
@@ -291,22 +322,34 @@ print(res_2.conf_int())
 
 predict = res_2.predict()
 predict = pd.DataFrame(predict.tail(20))
-predict.rename(columns ={0: 'Predicted'}, inplace=True)
-predict.rename(columns ={0: 'Predicted'}, inplace=True)
-combine = pd.concat([predict, data['forecast_variable'].tail(20)], axis=1)
+predict.rename(columns={0: "Predicted"}, inplace=True)
+predict.rename(columns={0: "Predicted"}, inplace=True)
+combine = pd.concat([predict, data["forecast_variable"].tail(20)], axis=1)
 combine = combine.reset_index()
-combine
+print(combine)
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=combine['Date'],y=combine['forecast_variable'],
-                         name="Actual Values (Adj Close"))
+fig.add_trace(
+    go.Scatter(
+        x=combine["Date"],
+        y=combine["forecast_variable"],
+        name="Actual Values (Adj Close",
+    )
+)
 
-fig.add_trace(go.Scatter(x=combine['Date'],y=combine['Predicted'],
-                         name="Predicted return"))
+fig.add_trace(
+    go.Scatter(x=combine["Date"], y=combine["Predicted"], name="Predicted return")
+)
 
-fig.update_layout(title="Nasdaq Actual vs Predicted values",
-   yaxis_title="Price ($)",
-    font=dict(family="Courier New, monospace",size=18,color="#7f7f7f"))
-fig.update_layout(autosize=False,width=800,height=500,)
+fig.update_layout(
+    title="Nasdaq Actual vs Predicted values",
+    yaxis_title="Price ($)",
+    font=dict(family="Courier New, monospace", size=18, color="#7f7f7f"),
+)
+fig.update_layout(
+    autosize=False,
+    width=800,
+    height=500,
+)
 fig.update_layout(legend_orientation="h")
 fig.show()
